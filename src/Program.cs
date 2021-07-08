3b1882d405ab9c904e4472216ca7df704fda013e
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-07-08 10:10:51 +0300

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace calculate_yiq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getContrastYIQ("#212121"));
+        }
+
+        static string getContrastYIQ(string hexColor)
+        {
+            var color = ColorTranslator.FromHtml(hexColor);
+            var yiq = ((color.R * 299) + (color.G * 587) + (color.B * 114)) / 1000;
+
+            return yiq >= 128 ? "#000000" : "#FFFFFF";
+        }
+    }
+}
